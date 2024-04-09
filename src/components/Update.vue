09e22016b267a680b47a40d9184dce74c8797460
Patrick Wilson <patrickafriyiewilson12345@gmail.com>
2024-04-09 20:51:51 +0000

    Udate methods and add method

---
+++ src/components/Update.vue
@@ -0,0 +1,37 @@
+<template>
+  <Header />
+  <h1>Hello User, Welcome on Update Restaurant Page</h1>   
+  <form class="add">
+    <input type="text" name="name" placeholder="Enter Name" v-model="restaurant.name" />
+    <input type="text" name="address" placeholder="Enter address" v-model="restaurant.address" />
+    <input type="text" name="contact" placeholder="Enter contact" v-model="restaurant.contact" />
+    <button type="button" v-on:click="addRestuarant">Add new Restaurant</button>
+  </form>
+</template>
+<script>
+import Header from './Header.vue'
+export default {
+ name: 'Update-vue',
+  components:{ 
+     Header
+},
+data()
+  {
+    return {
+      restaurant: {
+        name: '',
+        address: '',
+        contact: ''
+      }
+    }
+  },
+   mounted()
+      {
+        let user = localStorage.getItem('user-info');
+        if(!user)
+        {
+            this.$router.push({name:'Login'})
+        }
+      }
+ }
+</script> 
