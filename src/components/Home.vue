516fd0794788427afb74292d19ccbbf37734cb47
Patrick Wilson <patrickafriyiewilson12345@gmail.com>
2024-04-10 06:45:50 +0000

    Final setup

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,51 +1,71 @@
 <template>
   <Header />
-  <h1>Hello {{name}}, Welcome on Home Page</h1>   
+  <h1>Hello {{ name }}, Welcome on Home Page</h1>
   <table border="1">
     <tr>
       <td>Id</td>
       <td>Name</td>
       <td>Contact</td>
       <td>Address</td>
+      <td>Actions</td>
     </tr>
     <tr v-for="item in restaurant" :key="item.id">
-      <td>{{item.id}}</td>
-      <td>{{item.name}}</td>
-      <td>{{item.contact}}</td>
-      <td>{{item.address}}</td>
+      <td>{{ item.id }}</td>
+      <td>{{ item.name }}</td>
+      <td>{{ item.contact }}</td>
+      <td>{{ item.address }}</td>
+      <td>
+        <router-link :to="'/update/'+item.id">Update</router-link>
+        <button v-on:click="deleteRestuarant(item.id)">Delete</button>
+      </td>
     </tr>
   </table>
 </template>
-<script>
+<script>                                         
 import Header from './Header.vue';
 import axios from 'axios';
 export default {
- name: 'Home-vue',
- data() {
-  return {
-    name: '', 
-    restaurant:[],
-  }
- },   
-  components:{ 
-     Header
-},
-    async mounted()
+  name: 'Home-vue',
+  data() {
+    return {
+      name: '',
+      restaurant: [],
+    }
+  },
+  components: {
+    Header
+  },
+  methods: {
+    async deleteRestuarant(id)   
+    {
+      let result = await axios.delete("http://localhost:3000/restaurant/"+id);
+      console.warn(result)
+      if (result.status==200) 
       {
-        let user = localStorage.getItem('user-info');
-        this.name= JSON.parse(user).name;
-        if(!user)
-        {
-            this.$router.push({name:'SignUp'})
-        }
-        let result = await axios.get("http://localhost:3000/restaurant");
-        console.warn(result);
-         this.restaurant=result.data;
+        this.loadData()
       }
- }
-</script> 
+    },
+    async loadData() 
+    {
+      let user = localStorage.getItem('user-info');
+      this.name = JSON.parse(user).name;
+      if (!user) 
+      {
+        this.$router.push({ name: 'SignUp' })
+      }
+      let result = await axios.get("http://localhost:3000/restaurant");
+      this.restaurant = result.data;
+    }
+  },
+    async mounted() 
+    {
+      this.loadData();
+    },
+  }
+</script>
+
 <style>
-td{
+td {
   width: 160px;
   height: 40px;
 }
